8238f873126725bf83e7edbfae32bb8e275025a0
Vladislav Zagvostkin <vzagvostkin@cic.kz>
2025-10-01 16:58:17 +0500

    feature(login.tsx): login page #draft

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,5 +1,66 @@
+import {
+  Button,
+  EmailInput,
+  PasswordInput,
+} from '@krgaa/react-developer-burger-ui-components';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './login.module.css';
+
 const Login = (): React.JSX.Element => {
-  return <div>Страница входа</div>;
+  const [password, setPassword] = useState('password');
+  const [email, setEmail] = useState('demo@test.kz');
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setPassword(e.target.value);
+  };
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setEmail(e.target.value);
+  };
+  return (
+    <main className={`${styles.main} pl-4 pr-4`}>
+      <div className={styles.login_wrapper}>
+        <h1 className="text text_type_main-medium">Вход</h1>
+        <EmailInput
+          onChange={onChangeEmail}
+          value={email}
+          name={'email'}
+          isIcon={false}
+          extraClass="mt-6"
+        />
+        <PasswordInput
+          onChange={onChangePassword}
+          value={password}
+          name={'password'}
+          extraClass="mt-6"
+        />
+        <Button
+          htmlType="button"
+          type="primary"
+          size="medium"
+          extraClass="mt-6 margin-auto-x"
+        >
+          Войти
+        </Button>
+        <div
+          className={`${styles.login_footer} mt-20 text text_type_main-default text_color_inactive`}
+        >
+          <p>
+            Вы - новый пользователь?
+            <Link className={`${styles.link} ml-2`} to="/register">
+              Зарегистрироваться
+            </Link>
+          </p>
+          <p>
+            Забыли пароль?
+            <Link className={`${styles.link} ml-2`} to="/reset-password">
+              Восстановить пароль
+            </Link>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Login;
